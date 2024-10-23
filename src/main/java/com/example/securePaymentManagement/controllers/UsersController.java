f811ececb5dd542d0ed3feac058961c26c869129
guembygit <sergeguemby26@gmail.com>
2024-10-23 12:40:30 +0200

    edit user and delete

--- src/main/java/com/example/securePaymentManagement/controllers/UsersController.java
+++ src/main/java/com/example/securePaymentManagement/controllers/UsersController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.securePaymentManagement.Dto.UserDto;
 import com.example.securePaymentManagement.Models.User;
@@ -63,6 +64,55 @@ public class UsersController {
 		
 		repo.save(user);
 		
+		return "redirect:/users";
+	}
+	
+	@GetMapping("/edit")
+	public String showEditpage(Model model, @RequestParam  long id) {
+		
+		User user = repo.findById(id).get();
+		
+		UserDto userDto = new UserDto();
+		userDto.setLastName(user.getLastName());
+		userDto.setFirstName(user.getFirstName());
+		userDto.setEmail(user.getEmail());
+		userDto.setAddress(user.getAddress());
+		userDto.setNumber(user.getNumber());
+		userDto.setPasswords(user.getPasswords());
+		
+		model.addAttribute("userDto", userDto);
+		
+		return"users/edit";
+	}
+	
+	@PostMapping("/edit")
+	public String updateUser(Model model,
+			@RequestParam long id,
+			@Valid @ModelAttribute UserDto userDto, 
+			BindingResult result){
+		if(result.hasErrors()) {
+			return "users/edit";
+		}
+		
+		User user = repo.findById(id).get();
+		
+		user.setLastName(userDto.getLastName());
+		user.setFirstName(userDto.getFirstName());
+		user.setEmail(userDto.getEmail());
+		user.setAddress(userDto.getAddress());
+		user.setNumber(userDto.getNumber());
+		user.setPasswords(userDto.getPasswords());
+		
+		repo.save(user);
+		return "redirect:/users";
+	}
+	
+	@GetMapping("/delete")
+	public String deleteUser(@RequestParam long id){
+		
+		User user = repo.findById(id).get();
+		
+		repo.delete(user);
 		return "redirect:/users";
 	}
 }
